ea4bd8f722a94d6ec2b7811b2dde7c62da2bd21e
ArtemShchuchev <shuaa@rambler.ru>
2023-03-13 23:50:01 +0700

    программа

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-﻿#include "SecondaryFunction.h"
+﻿#include <chrono>
+#include <thread>
+#include "SecondaryFunction.h"
 
 /*
 Вам необходимо создать приложение, имитирующее очередь в окошко.
@@ -11,24 +13,42 @@
 клиентов и уменьшает его на 1. «Операционист» работает до последнего клиента.
 */
 
+void client(int& n)
+{
+	using namespace std::chrono_literals;
+	do
+	{
+		std::this_thread::sleep_for(1s);
+		++n;
+		std::wcout << L"Клиент +1: " << n << "\n";
+	} while (n < 5);
+}
+void operat(int& n)
+{
+	using namespace std::chrono_literals;
+	do
+	{
+		std::this_thread::sleep_for(2s);
+		--n;
+		std::wcout << L"Клиент -1: " << n << "\n";
+	} while (n > 0);
+}
+
 int main(int argc, char** argv)
 {
 	printHeader(L"Очередь клиентов");
+
+	std::wcout << L"Кол-во аппаратных вычислителей: " << std::thread::hardware_concurrency() << "\n";
+	std::wcout << L"Номер потока #id: " << std::this_thread::get_id() << "\n";
+
+	int clientCount(0);
+
+	// минус почему то не с каждым запуском работает??!
+	std::thread t1(client, std::ref(clientCount));
+	std::thread t2(operat, std::ref(clientCount));
+	t1.join();
+	t2.join();
 	
-	try
-	{
-		
-	}
-	catch (std::exception const& err)
-	{
-		consoleCol(col::br_red);
-		std::wcout << L"\nОшибка типа: " << typeid(err).name() << "\n";
-
-		std::wcerr << L"\nСообщение об ошибке:\n";
-		std::wcerr << utf2wide(err.what()) << '\n';
-		consoleCol(col::cancel);
-	}
-
 	std::wcout << "\n";
 
 	return 0;
